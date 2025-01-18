5c70c357f63e528216fad92794812a61c0329c27
unknown <najdenovviktor024@gmail.com>
2025-01-18 09:33:52 +0200

    proba3

--- Malka bukva v golqma.c
+++ Malka bukva v golqma.c
@@ -2,7 +2,7 @@
 void main()
 {
     char c;
-    printf("Enter random symbol");
+    printf("Enter random symbol debel");
     scanf("%c",&c);
     if(c>='a'&&c<='z'){c=c-32;printf("%c",c);}
     else printf("%c",c);}
